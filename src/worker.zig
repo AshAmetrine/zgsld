eb29b4b045261c084618909ed5fbe4bfd25f2042
Ash Ametrine <ashu9s@proton.me>
2026-02-21 14:17:53 +0000

    refactor: extract session env setup into worker/env

--- src/worker.zig
+++ src/worker.zig
@@ -1,10 +1,9 @@
 const std = @import("std");
 const zgipc = @import("ipc.zig");
 const pam_mod = @import("pam");
-const utils = @import("utils.zig");
 const greeter_mod = @import("worker/greeter.zig");
 const session_mod = @import("worker/session.zig");
-const UserInfo = utils.UserInfo;
+const env_mod = @import("worker/env.zig");
 
 const Pam = pam_mod.Pam;
 const PamMessages = pam_mod.Messages;
@@ -292,51 +291,8 @@ fn startSession(
     session_envmap: *std.process.EnvMap,
     vt: ?u8,
 ) !Session {
-    const pw = std.c.getpwnam(user) orelse return error.UserUnknown;
-
-    if (session_envmap.get("XDG_CURRENT_DESKTOP")) |v| {
-        try pam.putEnvAlloc("XDG_CURRENT_DESKTOP", v);
-    }
-    if (session_envmap.get("XDG_SESSION_DESKTOP")) |v| {
-        try pam.putEnvAlloc("XDG_SESSION_DESKTOP", v);
-    }
-    if (session_envmap.get("XDG_SESSION_TYPE")) |v| {
-        try pam.putEnvAlloc("XDG_SESSION_TYPE", v);
-    }
-
-    if (vt) |vt_num| {
-        var vt_buf: [3]u8 = undefined;
-        const vt_value = try std.fmt.bufPrint(&vt_buf, "{d}", .{vt_num});
-        try pam.putEnvAlloc("XDG_VTNR", vt_value);
-    }
-
-    try pam.putEnv("XDG_SESSION_CLASS=user");
-
-    if (std.posix.geteuid() == 0) {
-        try pam.openSession(.{});
-    }
-
-    // Add pam env list to the envmap (overwrites)
-    try pam.addEnvListToMap(session_envmap);
-
-    if (pw.dir) |home_dir| {
-        const s = std.mem.span(home_dir);
-        try session_envmap.put("HOME", s);
-        try session_envmap.put("PWD", s);
-        std.posix.chdir(s) catch {};
-    }
-
-    try session_envmap.put("USER", user);
-    try session_envmap.put("LOGNAME", user);
-    if (pw.shell) |shell| {
-        try session_envmap.put("SHELL", std.mem.span(shell));
-    }
-
-    const user_info: UserInfo = .{
-        .username = user,
-        .uid = pw.uid,
-        .gid = pw.gid,
-    };
+    try env_mod.applyPamSessionEnv(pam, session_envmap, vt);
+    const user_info = try env_mod.applyUserEnv(session_envmap, user);
     return Session.spawn(allocator, .{
         .session_info = info,
         .envmap = session_envmap,
---
+++ src/worker/env.zig
@@ -0,0 +1,58 @@
+const std = @import("std");
+const utils = @import("../utils.zig");
+
+const UserInfo = utils.UserInfo;
+
+pub fn applyPamSessionEnv(
+    pam: anytype,
+    session_envmap: *std.process.EnvMap,
+    vt: ?u8,
+) !void {
+    if (session_envmap.get("XDG_CURRENT_DESKTOP")) |v| {
+        try pam.putEnvAlloc("XDG_CURRENT_DESKTOP", v);
+    }
+    if (session_envmap.get("XDG_SESSION_DESKTOP")) |v| {
+        try pam.putEnvAlloc("XDG_SESSION_DESKTOP", v);
+    }
+    if (session_envmap.get("XDG_SESSION_TYPE")) |v| {
+        try pam.putEnvAlloc("XDG_SESSION_TYPE", v);
+    }
+
+    if (vt) |vt_num| {
+        var vt_buf: [3]u8 = undefined;
+        const vt_value = try std.fmt.bufPrint(&vt_buf, "{d}", .{vt_num});
+        try pam.putEnvAlloc("XDG_VTNR", vt_value);
+    }
+
+    try pam.putEnv("XDG_SESSION_CLASS=user");
+
+    if (std.posix.geteuid() == 0) {
+        try pam.openSession(.{});
+    }
+
+    // Add pam env list to the envmap (overwrites)
+    try pam.addEnvListToMap(session_envmap);
+}
+
+pub fn applyUserEnv(session_envmap: *std.process.EnvMap, user: [:0]const u8) !UserInfo {
+    const pw = std.c.getpwnam(user) orelse return error.UserUnknown;
+
+    if (pw.dir) |home_dir| {
+        const s = std.mem.span(home_dir);
+        try session_envmap.put("HOME", s);
+        try session_envmap.put("PWD", s);
+        std.posix.chdir(s) catch {};
+    }
+
+    try session_envmap.put("USER", user);
+    try session_envmap.put("LOGNAME", user);
+    if (pw.shell) |shell| {
+        try session_envmap.put("SHELL", std.mem.span(shell));
+    }
+
+    return .{
+        .username = user,
+        .uid = pw.uid,
+        .gid = pw.gid,
+    };
+}
