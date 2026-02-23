e551a0a626766623b00474efd2830db19bb37a12
Ash Ametrine <ashu9s@proton.me>
2026-02-23 12:35:49 +0000

    build: switch clap/zigini to lazyDependency

--- build.zig
+++ build.zig
@@ -27,9 +27,7 @@ pub fn build(b: *std.Build) !void {
     build_options.addOption(bool, "x11_support", x11_enabled);
     const build_options_mod = build_options.createModule();
 
-    const clap = b.dependency("clap", .{ .target = target, .optimize = optimize });
     const pam = b.dependency("pam", .{ .target = target, .optimize = optimize });
-    const zigini = b.dependency("zigini", .{ .target = target, .optimize = optimize });
     const ipc_mod = b.createModule(.{
         .root_source_file = b.path("src/ipc.zig"),
         .target = target,
@@ -47,54 +45,59 @@ pub fn build(b: *std.Build) !void {
         },
     });
 
-    const exe = b.addExecutable(.{
-        .name = "zgsld",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "build_options", .module = build_options_mod },
-                .{ .name = "clap", .module = clap.module("clap") },
-                .{ .name = "ipc", .module = ipc_mod },
-                .{ .name = "zigini", .module = zigini.module("zigini") },
-                .{ .name = "pam", .module = pam.module("pam") },
-            },
-            .link_libc = true,
-        }),
-    });
+    if (b.pkg_hash.len == 0) {
+        const clap = b.lazyDependency("clap", .{ .target = target, .optimize = optimize }) orelse return;
+        const zigini = b.lazyDependency("zigini", .{ .target = target, .optimize = optimize }) orelse return;
 
-    b.installArtifact(exe);
+        const exe = b.addExecutable(.{
+            .name = "zgsld",
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/main.zig"),
+                .target = target,
+                .optimize = optimize,
+                .imports = &.{
+                    .{ .name = "build_options", .module = build_options_mod },
+                    .{ .name = "clap", .module = clap.module("clap") },
+                    .{ .name = "ipc", .module = ipc_mod },
+                    .{ .name = "zigini", .module = zigini.module("zigini") },
+                    .{ .name = "pam", .module = pam.module("pam") },
+                },
+                .link_libc = true,
+            }),
+        });
 
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
+        b.installArtifact(exe);
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        // This allows the user to pass arguments to the application in the build
+        // command itself, like this: `zig build run -- arg1 arg2 etc`
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
+
+        const exe_unit_tests = b.addTest(.{
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/main.zig"),
+                .target = target,
+                .optimize = optimize,
+                .imports = &.{
+                    .{ .name = "build_options", .module = build_options_mod },
+                    .{ .name = "clap", .module = clap.module("clap") },
+                    .{ .name = "ipc", .module = ipc_mod },
+                },
+            }),
+        });
+
+        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_exe_unit_tests.step);
     }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "build_options", .module = build_options_mod },
-                .{ .name = "clap", .module = clap.module("clap") },
-                .{ .name = "ipc", .module = ipc_mod },
-            },
-        }),
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
 
     const fmt_step = b.step("fmt", "Format source files");
     const fmt_cmd = b.addFmt(.{ .paths = &.{ "build.zig", "build.zig.zon", "src" } });
