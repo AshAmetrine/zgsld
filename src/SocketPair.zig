834bf08d59b2dd2bcd8335f946646135f80b8839
Ash Ametrine <me@ashametrine.dev>
2026-03-27 09:03:01 +0000

    fix(socketpair): close descriptors if cloexec setup fails

--- src/SocketPair.zig
+++ src/SocketPair.zig
@@ -9,6 +9,8 @@ pub fn init(parent_cloexec: bool) !SocketPair {
     var fds: [2]std.posix.fd_t = undefined;
     const rc = std.c.socketpair(std.posix.AF.UNIX, std.posix.SOCK.STREAM, 0, &fds);
     if (rc != 0) return std.posix.unexpectedErrno(std.posix.errno(rc));
+    errdefer std.posix.close(fds[0]);
+    errdefer std.posix.close(fds[1]);
 
     if (parent_cloexec) {
         const flags = try std.posix.fcntl(fds[0], std.posix.F.GETFD, 0);
